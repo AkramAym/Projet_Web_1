274ed39004888cb660ef0b8c069b91337ba3ed70
YaniAgoune <6256085@bdeb.qc.ca>
2025-02-17 10:14:38 -0500

    Liaison du serveur à la base de données pour l'index

--- script_bd.sql
+++ script_bd.sql
@@ -203,3 +203,34 @@ ALTER TABLE tome
     ADD CONSTRAINT tome_serie_fk 
     FOREIGN KEY (serie_id_serie) 
     REFERENCES serie(id_serie);
+
+DROP USER 'scott'@'%';
+CREATE USER 'scott'@'%' IDENTIFIED WITH mysql_native_password BY 'oracle';
+GRANT ALL PRIVILEGES ON *.* TO 'scott'@'%';
+FLUSH PRIVILEGES;
+
+INSERT INTO categorie (nom_categorie, description)
+VALUES 
+    ('Shonen', 'Le Shonen est un genre de manga principalement destiné à un public jeune masculin. Il se caractérise par des histoires dynamiques, souvent axées sur l\'action, l\'aventure, l\'amitié et la persévérance. Exemples populaires : "Naruto", "Dragon Ball", "One Piece".'),
+    ('Seinen', 'Le Seinen est un genre de manga ciblant un public adulte masculin. Les thèmes abordés sont souvent plus matures et complexes, incluant la psychologie, la politique, la violence ou la science-fiction. Exemples populaires : "Berserk", "Tokyo Ghoul", "Ghost in the Shell".'),
+    ('Shojo', 'Le Shojo est un genre de manga destiné à un public jeune féminin. Il met souvent l\'accent sur les relations interpersonnelles, les romances, les drames et les émotions. Le style artistique est généralement plus raffiné et expressif. Exemples populaires : "Fruits Basket", "Nana", "Sailor Moon".');
+
+INSERT INTO serie (titre_serie, auteur, editeur, synopsis, categorie_id_categorie)
+VALUES (
+    'Jujutsu Kaisen', 
+    'Gege Akutami', 
+    'Shūeisha', 
+    'Plus de 10 000 morts et disparus sont recensés chaque année au Japon. Les sentiments négatifs que relâchent les êtres humains sont en cause. Souffrance, regrets, humiliation : leur concentration dans un même endroit engendre des malédictions souvent mortelles... C''est ce que va découvrir Yuji Itadori, lycéen et membre du club d''occultisme. Il ne croit pas aux fantômes, mais il est doté d''une force physique hors norme qui représente un véritable atout pour les missions du groupe... jusqu''à ce que l''une d''elles prenne une mauvaise tournure. La relique qu''ils dénichent, le doigt découpé d''un démon millénaire, attire les monstres ! Sans réfléchir : le jeune homme avale la relique pour briser la malédiction ! Maintenant, il se trouve possédé par Ryômen Sukuna, le célèbre démon à deux visages. Cependant, contre toute attente, Yuji est toujours capable de garder le contrôle de son corps. Mais en dépit de cela, il est condamné à mort par l''organisation des exorcistes... Une décision qui ne pourra être repoussée qu''à une seule condition : trouver tous les doigts de Sukuna afin d''écarter la menace une bonne fois pour toutes !', 
+    1
+);
+
+INSERT INTO tome (isbn, numero_volume, annee_publication, serie_id_serie, prix, image, stock)
+VALUES (
+    9791032705544, 
+    1, 
+    '2020-07-21', 
+    1, 
+    16.00, 
+    '/images/kaisen.jpg', 
+    5
+);
