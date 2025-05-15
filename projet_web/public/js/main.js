6c7aeb06af0c2d2107f1bc6f95e63e360099c0c5
AkramAym <2391145@bdeb.qc.ca>
2025-05-15 19:49:53 -0400

    ajout d'un style pour le message d'erreur

--- projet_web/public/js/main.js
+++ projet_web/public/js/main.js
@@ -1,15 +1,28 @@
 
 // Gestion de l'affichage du mot de passe
-document.querySelectorAll('.toggle-password').forEach(item => {
-    item.addEventListener('click', function() {
-        let passwordField = this.previousElementSibling;
-        if (passwordField.type === "password") {
-            passwordField.type = "text";
-        } else {
-            passwordField.type = "password";
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  const toggles = document.querySelectorAll(".toggle-password");
+
+  toggles.forEach(toggle => {
+    toggle.addEventListener("click", function () {
+      // Trouve l'input dans le même parent
+      const input = this.closest('.password-container').querySelector('input');
+      const icon = this.querySelector("i");
+
+      if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+      } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+      }
     });
+  });
 });
+
+
 // Fonction de validation du formulaire
 function validerFormulaire() {
     let email = document.getElementById("email").value;
